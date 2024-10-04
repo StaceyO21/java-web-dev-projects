a831b93e2383092cadb37c39d1b87399fc8126ce
StaceyO21 <staceyobrien21@gmail.com>
2024-10-03 20:05:38 -0500

    Classes part one Studio

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
@@ -3,6 +3,10 @@ package org.launchcode;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MenuItem item1 = new MenuItem("Dal", 30, "Main Course", "10/03/2024");
+        MenuItem item2 = new MenuItem("English Toffee", 15, "Dessert", "10/03/2024");
+        MenuItem item3 = new MenuItem("Fried Plantains", 18, "Small Plate", "10/03/2024");
+
+
     }
 }
---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,10 @@
+package org.launchcode;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Menu {
+    ArrayList<String> items = new ArrayList<>();
+
+
+}
