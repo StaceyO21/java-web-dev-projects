d1b22f8708449fc6c356170647c8ad04e9c24b9c
StaceyO21 <staceyobrien21@gmail.com>
2024-10-17 19:37:26 -0500

    unit-testing Java

--- unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
+++ unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
@@ -1,14 +1,8 @@
 package org.launchcode;
 public class BalancedBrackets {
     /*
-     * The function BalancedBrackets should return true if and only if
-     * the input string has a set of "balanced" brackets.
+
      *
-     * That is, whether it consists entirely of pairs of opening/closing
-     * brackets (in that order), none of which mis-nest. We consider a bracket
-     * to be square-brackets: [ or ].
-     *
-     * The string may contain non-bracket characters as well.
      *
      * These strings have balanced brackets:
      *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
@@ -27,7 +21,10 @@ public class BalancedBrackets {
             } else if (ch == ']') {
                 brackets--;
             }
+            if (brackets < 0) {
+                break;
+            }
         }
         return brackets == 0;
-    }
+        }
 }
