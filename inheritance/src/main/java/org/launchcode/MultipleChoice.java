9a86612898dff8d5f2596b1274853bb1159964db
StaceyO21 <staceyobrien21@gmail.com>
2024-11-13 17:13:38 -0600

    Inheritance Studio - Quiz

--- inheritance/src/main/java/org/launchcode/MultipleChoice.java
+++ inheritance/src/main/java/org/launchcode/MultipleChoice.java
@@ -1,4 +1,47 @@
 package org.launchcode;
 
-public class MultipleChoice {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultipleChoice extends Question{
+
+    private ArrayList<String> possibleAnswers;
+    private int correctAnswer;
+
+    public MultipleChoice(int pointValue, String text, int correctAnswer, ArrayList<String> possibleAnswers) {
+        super(pointValue, text);
+        this.correctAnswer = correctAnswer;
+        this.possibleAnswers = possibleAnswers;
+        setPointValue(1);
+    }
+
+    @Override
+    public void displayAnswers() {
+        for(int i = 0; i < possibleAnswers.size(); i++) {
+            System.out.println(possibleAnswers.get(i));
+        }
+}
+
+    public boolean isCorrectAnswer(int possibleAnswer) {
+        if (possibleAnswer == correctAnswer) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public int getAnswers() {
+        Scanner answer = new Scanner(System.in);
+        System.out.println("What is your answer? Enter the number of the option you believe is correct.");
+        String userAnswer = answer.nextLine();
+        int result = Integer.parseInt(userAnswer);
+        if(isCorrectAnswer(result)) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
 }
--- inheritance/src/main/java/org/launchcode/Question.java
+++ inheritance/src/main/java/org/launchcode/Question.java
@@ -1,4 +1,28 @@
 package org.launchcode;
 
-public class Question {
+public abstract class Question {
+
+    private int pointValue;
+    private String text;
+
+    public Question(int pointValue, String text) {
+    this.pointValue = pointValue;
+    this.text = text;
+}
+
+    public void displayQuestion() {
+        System.out.println(text);
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    public void setPointValue(int x) {
+        this.pointValue = x;
+    }
+
+    public abstract void displayAnswers ();
+
+    public abstract int getAnswers();
 }
